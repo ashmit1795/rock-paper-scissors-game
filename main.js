699431a4bea9bb5c4d8de9ad8ec224b966395268
ashmit1795 <139261819+ashmit1795@users.noreply.github.com>
2023-12-29 00:22:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
+//Global Scope Variables:
 let userScore = 0; //Tracking the User Score
 let compScore = 0; //Tracking the Computer Score
 let gameTurn = 0; //Tracking the no. of turns played
 let message = document.querySelector(".msg-box"); //Accessing the message box
 let turn = document.querySelector("#turn"); //Accessing the turn counter box
-
+let compChoice; //Variable for storing computer choice
+let userChoice; //Variable for storing user choice
 
 //Configuartion for computer area after computer choice is generated
 let compRock = document.querySelector("#comp-Rock");
 let compPaper = document.querySelector("#comp-Paper");
 let compScissors = document.querySelector("#comp-Scissors");
-// let currBgColor = "none"
 
 const compConfig = (compChoice) =>{
         if (compChoice === "Rock"){
@@ -19,26 +20,7 @@ const compConfig = (compChoice) =>{
         }else if (compChoice === "Scissors"){
             compScissors.style.backgroundColor = "#081b31";
         }
-    // currBgColor = "coloured";
-    // if (currBgColor === "none"){
-    //     if (compChoice === "Rock"){
-    //         compRock.style.backgroundColor = "#081b31";
-    //     }else if (compChoice === "Paper"){
-    //         compPaper.style.backgroundColor = "#081b31";
-    //     }else if (compChoice === "Scissors"){
-    //         compScissors.style.backgroundColor = "#081b31";
-    //     }
-    // }else{
-    //     currBgColor = "none";
-    //     if (compChoice === "Rock"){
-    //         compRock.style.backgroundColor = "none";
-    //     }else if (compChoice === "Paper"){
-    //         compPaper.style.backgroundColor = "none";
-    //     }else if (compChoice === "Scissors"){
-    //         compScissors.style.backgroundColor = "none";
-    //     }
-    // }
-}
+    }
 
 //Computer Choice Generation
 const genCompChoice = () =>{
@@ -47,31 +29,38 @@ const genCompChoice = () =>{
     return compChoices[i];
 }
 
+//Display message for draw game
 const drawGame = () =>{
     console.log("It's a draw");
-    message.innerText = "It's a draw"
+    message.innerText = "It's a draw. Play  again.";
     message.style.backgroundColor = "gray";
 }
 
+//Display message for winner and score incrementer
 const showWinner = (res) =>{
     if (res === true){
+        //User Wins
+        console.log("User Wins!")
         userScore+=1;
         let user = document.querySelector("#your-score");
         user.innerText = userScore;
-        message.innerText = "You win, Computer lost";
+        message.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
         message.style.backgroundColor = "green";
     }else if (res === false){
+        //Computer Wins
+        console.log("Computer Wins!")
         compScore+=1;
         let computer = document.querySelector("#comp-score");
         computer.innerText = compScore;
-        message.innerText = "You lost, Computer win";
+        message.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
         message.style.backgroundColor = "red";
     }
 }
 
+//Function to play the game
 const playGame = (userChoice) =>{
     //To play the game, we need computer choice also
-    let compChoice = genCompChoice();
+    compChoice = genCompChoice();
     console.log(`Computer Choice is: ${compChoice}`); //This is to check in the console
 
     compConfig(compChoice); //Calling the function for configurating the computer area
@@ -95,12 +84,14 @@ const playGame = (userChoice) =>{
     }
 }
 
+//All Starts here:
 const choices = document.querySelectorAll(".choice");
 choices.forEach((choice)=>{
     // console.log(choice)
     choice.addEventListener("click", () => {
-        const userChoice = choice.getAttribute("id");
+        userChoice = choice.getAttribute("id");
         gameTurn += 1;
+        //Reseting the config of Computer Area after each turn
         compRock.style.backgroundColor = "#ffffff";
         compPaper.style.backgroundColor = "#ffffff";
         compScissors.style.backgroundColor = "#ffffff";
